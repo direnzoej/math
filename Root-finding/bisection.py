8f78255ddf64887cb462e24db24b24451ab32e05
Evan <37559700+Direnzoej@users.noreply.github.com>
2019-01-04 23:11:37 -0600

    Update bisection.py

--- Root-finding/bisection.py
+++ Root-finding/bisection.py
@@ -1,17 +1,26 @@
-def bisection(a,b,eps,tar=0):
-    def f(x):
-        """Defines the function to be used by bisection"""
-        return -1+3*x-3*x**2+x**3
-            
-    c = (a+b)/2
-    f_c = f(c)
-    print([a,b,c,f_c])#troubleshooting
-    if f_c == tar:
-        print('Possible underflow error. [a,b] returned.
-        print('c =', c,'and b - a =', b-a)
+def bisection(f,a,b,eps=1E-5,tar=0):
+    '''Applies the bisection method by recursion to find 
+    the root of a given function on a given interval.
+    
+    f (func): the function evaluated
+    a (flt): interval left endpoint
+    b (flt): interval right endpoint
+    eps (flt): 'epsilon', the greatest tolerated interval 
+                length, default set to 1E-5
+    tar (flt): the function value sought on the interval,
+                default set to 0 (roots mean f(x)=0...)
+    
+    Returns the interval containing the root when the 
+    interval is smaller than the given epsilon.
+    '''
+    c = (a+b)/2 # find interval midpoint
+    f_c = f(c) # evaluate f at midpoint
+    
+    if f_c == tar: # check for underflow
+        print('Possible underflow error. [a,b] returned.')
+        print(f'c = {c} and b - a = {b-a}')
         return [a,b]
-    if f_c > tar: b = c
-    if f_c < tar: a = c
-    if b - a < eps: return [a,b]
-    else: return bisection(a,b,eps,tar)
-            
+    if f_c > tar: b = c # set right endpoint to midpoint OR
+    if f_c < tar: a = c # set left endpoint to midpoint
+    if b - a < eps: return [a,b] # return solution
+    else: return bisection(f,a,b,eps,tar) # recursion to solve
