b8831028d4d9467fe875bf224fab680d74b515ee
Evan <37559700+Direnzoej@users.noreply.github.com>
2019-01-06 12:57:09 -0600

    Update recursive_addition.py

--- Operations/recursive_addition.py
+++ Operations/recursive_addition.py
@@ -4,12 +4,16 @@ Created on Mon Jun 11 21:58:54 2018
 
 @author: Evan
 """
-
-# build a function to compute addition by recursion
-
 def add(x, y):
-    if y == 1:
+    '''Adds two integers by recursion.
+    
+    x, y (int): the integers to add
+    
+    Returns an integer.
+    '''
+    if y == 1: # base case
         x += 1
         return x
-    elif y > 1: return 1 + add(x, y - 1)
-    elif y < 1: return -1 + add(x, y + 1)
+    # addition and subtraction recursions
+    elif y > 1: return 1 + add(x, y-1)
+    elif y < 1: return -1 + add(x, y+1)
